7e8325af161ca028733cd38f32072b6de7dc6674
Jerry <jerryjoekaizer@gmail.com>
2021-04-04 12:13:36 +0100

    cleared AndroidManifest error

--- android/app/src/main/java/co/appbrewery/dicee/MainActivity.java
+++ android/app/src/main/java/co/appbrewery/dicee/MainActivity.java
@@ -3,9 +3,6 @@ package co.appbrewery.dicee;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+
+
 }
