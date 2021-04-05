41e42309fd5cf7be7ace9be8477ca632452d0451
Jerry <jerryjoekaizer@gmail.com>
2021-04-05 10:50:50 +0100

    new features added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -7,7 +8,9 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(
+            child: Text('DICEE'),
+          ),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -16,17 +19,48 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  DicePage({Key key}) : super(key: key);
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int rightDie = 1;
+  int leftDie = 1;
+
+  void rollDie() {
+    setState(() {
+      rightDie = Random().nextInt(6) + 1;
+      leftDie = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image(
-            image: AssetImage ('image/dice1.png'),
+    return Center(
+      child: Row(
+        children: <Widget>[
+          
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                rollDie();
+              },
+              child: Image.asset('images/dice$rightDie.png'),
+            ),
           ),
-        ),
-      ],
+
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                rollDie();
+              },
+              child: Image.asset('images/dice$leftDie.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
